711f14e909784257037dd9aa9420f998b46f0b83
mcmaruhin-pixel <mcmaruhin@gmail.com>
2026-05-25 14:16:02 +0700

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,11 +1,11 @@
 const CACHE_NAME = 'psm-calculator-v1';
 const urlsToCache = [
-  '/',
-  '/index.html',
-  '/style.css',
-  '/script.js',
-  '/icon-192.png',
-  '/icon-512.png'
+  '/psm-calculator/',
+  '/psm-calculator/index.html',
+  '/psm-calculator/style.css',
+  '/psm-calculator/script.js',
+  '/psm-calculator/icon-192.png',
+  '/psm-calculator/icon-512.png'
 ];
 
 self.addEventListener('install', event => {
@@ -15,6 +15,14 @@ self.addEventListener('install', event => {
   );
 });
 
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
+});
+
+// Принудительное обновление (добавьте, если хотите)
 self.addEventListener('activate', event => {
   event.waitUntil(
     caches.keys().then(cacheNames => {
@@ -27,17 +35,5 @@ self.addEventListener('activate', event => {
       );
     })
   );
-  // Принудительно активируем нового SW
   self.clients.claim();
-  // Сообщаем всем открытым вкладкам, что нужно обновиться
-  self.clients.matchAll().then(clients => {
-    clients.forEach(client => client.postMessage('update-available'));
-  });
-});
-
-self.addEventListener('fetch', event => {
-  event.respondWith(
-    caches.match(event.request)
-      .then(response => response || fetch(event.request))
-  );
 });
